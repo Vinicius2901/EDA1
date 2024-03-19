8fb493b137f4f0938357efd2a6ee8c522f000ed4
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2024-03-19 10:52:37 -0300

    Add files via upload

--- listarev/6.c
+++ listarev/6.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+//Struct do meio da estrutura
 struct nodo {
     int x;
     struct nodo *link;
 }
+
+//Struct de inicio da estrutura
+typedef struct descritor {
+    int tamanho;
+    Nodo *inicio;
+} Descritor;
 
 //Contador ate nulo
 int cont1( struct *nodo ) {
@@ -13,7 +20,7 @@ int cont1( struct *nodo ) {
     while( aux != NULL ) {
         aux = aux->link;
         cont++;
-    }
+    }    free( p->inicio )
 
     return cont;
 }
@@ -34,7 +41,7 @@ int cont2( struct *nodo ) {
 }
 
 //Contador volta nele mesmo
-int cont2( struct *nodo ) {
+int cont3( struct *nodo ) {
     int cont = 0;
     struct nodo *p = nodo;
     struct nodo *aux = p
@@ -46,4 +53,41 @@ int cont2( struct *nodo ) {
     }
 
     return cont;
+}
+
+int reinicia( Descritor *p ) {
+    if( p-> tamanho == 0 )
+        return 0;
+
+    Nodo *nodo_atual = p->inicio;
+    Nodo *aux = p->inicio->link;
+
+    while( aux != NULL ) {
+        aux = nodo_atual->link;
+        free( nodo_atual )
+        nodo_atual = aux;
+    }
+
+    p->tamanho = 0;
+    p->inicio = NULL;
+    return 1;
+}
+
+int insere( Descritor *p, Nodo *novo, int pos ) {
+    int i;
+    Nodo *aux = NULL
+    if( pos > p->tamanho || pos < 1 || p->inicio == NULL )
+        return 0;
+    if( pos == 1 ) {
+        novo->link = p->inicio;
+        p->inicio = novo;
+    }
+    aux = p->inicio;
+    for( i = 1; i < pos-1; i++ )
+        aux = aux->link
+    novo->link = aux->link;
+    aux->link = novo;
+    (p->tamanho)++;
+
+    return 1;
 }
