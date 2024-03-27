e657a5d11334ffd5b1e5cdcf02c7d60fe3469270
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2024-03-27 11:32:37 -0300

    Create pilhas.c

---
+++ pilhas/pilhas.c
@@ -0,0 +1,36 @@
+/*
+    Ordem de saida:
+    E1<E2<E3
+
+    Lista encadeada:
+    descritor->a->b->c
+
+    algumas funções:
+    cria()
+    empilha()
+    desempilha()
+    reinicia()
+    buscaTopo()
+
+    encapsulamento:
+    modulo de aplicacao( Exemplo: Torre de Hanoi ) <-> interface <-> implementação interna( Exemplo: biblioteca sobre pilhas )
+
+    tipos de operação:
+    construtoras: cria uma estrutura de dados
+    acesso: ve informações( tipo ver o que esta no topo da pilha )
+    manipulação: muda algo da estrutura
+    destrutoras: remove da memoria a estrutura
+    
+    Documentação:
+    Pre-condição: infoma o q precisa para uma operação funcionar
+    Pos-consição: o que e como uma função devolve
+
+    Atomicidade: Caso uma operação falhe, tudo o que foi feito antes da operação falhar em q voltar ao q era entes
+    A atomicidade ocorre na interface
+
+    Consistencia: a operação não pode afetar o funcionamento da Estrutura de dados ou acrescentar informações incertas
+
+    
+
+    
+*/
