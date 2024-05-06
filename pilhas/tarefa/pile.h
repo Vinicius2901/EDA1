4edc29096572f439d576253e20acfccec989edd0
MaxHToviansky <mtoviansky@gmail.com>
2024-05-06 16:29:54 -0300

    Update & Remove pilha

--- pilhas/tarefa/pile.h
+++ pilhas/tarefa/pile.h
@@ -9,4 +9,14 @@ typedef struct MultiPile{
     Element *v;
 } MultiPile;
 
+MultiPile criaPilha( int n );
+
 void destroiPilha( MultiPile *p );
+
+int empilhai(MultiPile *p, int e );
+
+int empilhaf( MultiPile *p, float e );
+
+int cheia( MultiPile *p );
+
+int vazia( MultiPile *p);
--- pilhas/tarefa/pilha.c
+++ pilhas/tarefa/pilha.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "pile.h"
 
-MultiPile CriaPilha(int n){
+MultiPile criaPilha(int n){
     MultiPile pilha;
     pilha.tamVet = n;
     pilha.topo2 = n;
@@ -9,13 +9,54 @@ MultiPile CriaPilha(int n){
     pilha.v = malloc(n*sizeof(Element));
 }
 
-
-
 void destroiPilha(MultiPile *p){
     free(p->v);
     p->v = NULL;
     free(p);
     p = NULL;
 }
+
+int empilhai(MultiPile *p, int e ) {
+    if(cheia(p))
+        return 1;
+    p->topo1++;
+    Element tmp;
+    tmp.a = e;
+    p->v[p->topo1] = tmp;
+    return 0;
+}
+
+int empilhaf( MultiPile *p, float e ){
+    if( cheia(p) ) 
+        return 1;
+    p->topo2--;
+    Element tmp;
+    tmp.b = e;
+    p->v[p->topo2] = tmp;
+    return 0;
+}
+
+int vazia( MultiPile *p){
+    return p->topo1 == -1 && p->topo2 == p->tamVet;
+}
+
+int cheia( MultiPile *p ) {
+    return p->topo1 >= p->topo2;
+}
+
+
+Element desempilha(MultiPile *p, int s){
+    if(s==0 && p->topo1>=0){
+        p->topo1--;
+        return p->v[p->topo1+1];
+    }
+    if(s==1 && p->topo2<=p->tamVet){
+        p->topo2++;
+        return p->v[p->topo2-1];
+    }
+    Element error;
+    return error;
+}
+
 
 
