d39779a8f27ccc4181469062ebf2947689565cb6
MaxHToviansky <mtoviansky@gmail.com>
2024-05-06 15:58:31 -0300

    CRUD of multiPile
    Co-authored-by: Vinícius Giroti <Vinicius2901@users.noreply.github.com>

--- pilhas/tarefa/pile.h
+++ pilhas/tarefa/pile.h
@@ -2,4 +2,11 @@ typedef union element
 {
     int a;
     float b;
-};
+} Element;
+
+typedef struct MultiPile{
+    int topo1,topo2, tamVet;
+    Element *v;
+} MultiPile;
+
+void destroiPilha( MultiPile *p );
--- pilhas/tarefa/pilha.c
+++ pilhas/tarefa/pilha.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include "pile.h"
 
-typedef struct MultiPile{
-    int topo1,topo2, tamVet;
-    int v[];
-} MultiPile;
-
-typedef struct Pile{
-    
-} Pile;
-
-MultiPile CriaPilha(int n, void *p){
+MultiPile CriaPilha(int n){
     MultiPile pilha;
     pilha.tamVet = n;
     pilha.topo2 = n;
     pilha.topo1 = -1;
-    pilha.v = malloc(sizeof );
+    pilha.v = malloc(n*sizeof(Element));
 }
+
+
+
+void destroiPilha(MultiPile *p){
+    free(p->v);
+    p->v = NULL;
+    free(p);
+    p = NULL;
+}
+
+
