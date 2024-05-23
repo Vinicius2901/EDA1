97a3b1172d3ed2e5a659bfecf2b10385b536a761
Vinicius <vinicius.giroti@gmail.com>
2024-05-23 15:29:47 -0300

    Adicionando casos de teste e tratando tags de comentário.

--- pilhas/tarefab/main.c
+++ pilhas/tarefab/main.c
@@ -24,6 +24,7 @@ int main( int argc, char *argv[] ){
     char *s = malloc(1*sizeof(char)); // Alocação de memória da string que será analisada.
     s[0] = '\0';            // Inicialização da String para evitar "lixo" de memória.
     int i = 0, flag = 0;    // Contador para acessar o índice da string e booleano que indica quando a string está sendo formada.
+    int aux = 2;
 
     //------------ Funcionamento do código ----------------------------
 
@@ -74,18 +75,31 @@ int main( int argc, char *argv[] ){
             s[i-1] = c;                        // O penúltimo caracter da string é o caracter atual do arquivo.
             s[i] = '\0';                       // O último caracter da string é o fechamento da String.
 
-            // // Tratamento da tag de comentário.
+            // Tratamento da tag de comentário.
 
-            // // Verificação se a string atual é igual ao início da tag de comentário ou ao fim
-            // if( !strcmp( s, "!--") || !strcmp( s, "--") ) {
-            // 	flag = 0;                       // A string deixa de ser formatada.
-            //     while ( c != '>' ) {
-            //         c = fgetc(f);               // Caracter é pulado até chegar no fechamento da tag de comentário ou de alguma tag dentro.
-            //     }
-            //     c = fgetc(f);                   // 'c' passa a ser um caracter a frente no arquivo.
-            //     s = realloc(s, sizeof(char));   // Realocação da memória da string reiniciá-la.
-            //     i = 0;                          // Indice da string = 0.
-            // }
+            // Verificação se a string atual é igual ao início da tag de comentário.
+            if( !strcmp( s, "!--") ) {
+            	flag = 0;                       // A string deixa de ser formatada.
+                aux = 2;                        // Auxiliar que indica o último caracter da string.
+                s = realloc(s, 4*sizeof(char)); // Realocação da memória para a string ter pelo menos 4 de espaço disponível.
+                for( i = 0; i < 4; i++ )
+                    s[i] = '\0';                // Inicialização de todos os termos da string em '\0'.
+                i = 0;
+                while ( s[aux] != '>' && s[aux-1] != '-' && s[aux-2] != '-' ) { // Enquanto a tag não ser fechada, o loop continuará sendo executado.
+                    c = fgetc(f);                                 // Caracter é pulado até chegar no fechamento da tag de comentário ou de alguma tag dentro.
+                    if( c == '-' || c == '>' ) {                  // Verificação se o caracter atual é '-' ou '>'
+                        i++;
+                        if( i > 3 ) {
+                            s = realloc( s, (i+1)*sizeof(char) ); // Se houver mais de dois '-' ou '>' e não serem de fechamento, a string terá mais memória alocada.
+                            aux++;
+                        }
+                        s[i-1] = c;                               // É adicionada a string '-' ou '>'.
+                        s[i] = '\0';                              // Sempre adicionando o fechamento de string para não gerar lixo.
+                    }
+                }
+                s = realloc(s, sizeof(char));   // Realocação da memória da string reiniciá-la.
+                i = 0;                          // Indice da string = 0.
+            }
         }
         
         // Verificação de quando a string deve começar a ser formatada.
